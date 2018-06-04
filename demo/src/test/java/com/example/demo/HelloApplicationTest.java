c5de3d055f7bd1c4632583a0797da2a735aea719
Rock Lee <rockleeprc@outlook.com>
2018-06-04 23:09:33 +0800

    第一个springboot项目

---
+++ demo/src/test/java/com/example/demo/HelloApplicationTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.web.HelloController;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)//引入spring4对junit的支持
+@SpringBootTest(classes = DemoApplication.class)//指定springboot启动类
+@WebAppConfiguration//模拟ServletContext
+public class HelloApplicationTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void hello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string(Matchers.equalTo("hello spring boot")));
+    }
+}
