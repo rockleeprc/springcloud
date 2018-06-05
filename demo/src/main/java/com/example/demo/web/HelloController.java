096c627923780afac9d279c3f5be41260e76c3e9
rockleeprc <rockleeprc@outlook.com>
2018-06-05 17:56:10 +0800

    eureka

--- demo/src/main/java/com/example/demo/web/HelloController.java
+++ demo/src/main/java/com/example/demo/web/HelloController.java
@@ -1,16 +1,34 @@
 package com.example.demo.web;
 
 import com.example.demo.po.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+
+    @Autowired
+    private DiscoveryClient clinet;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        Book b = new Book();
-        System.out.println(b.getName());
-        System.out.println(b.getAuthor());
+        ServiceInstance instance = clinet.getLocalServiceInstance();
+        System.out.println(instance.getHost());
+        System.out.println(instance.getServiceId());
         return "hello spring boot";
     }
+    @RequestMapping(value = "/info", method = RequestMethod.GET)
+    public String info() {
+        ServiceInstance instance = clinet.getLocalServiceInstance();
+        System.out.println(instance.getHost());
+        System.out.println(instance.getServiceId());
+        return "hello spring info";
+    }
+
 }
