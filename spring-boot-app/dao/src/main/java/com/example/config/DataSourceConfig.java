ed9ef64f6f93181c6e299136ed58f177b354d151
rockleeprc <rockleeprc@outlook.com>
2018-06-20 18:20:38 +0800

    eidt

--- spring-boot-app/dao/src/main/java/com/example/config/DataSourceConfig.java
+++ spring-boot-app/dao/src/main/java/com/example/config/DataSourceConfig.java
@@ -2,6 +2,7 @@ package com.example.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -15,6 +16,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @PropertySource(value = "classpath:dao.properties")
+@ConditionalOnMissingBean(DataSource.class)
 public class DataSourceConfig {
     /**
      * 配置文件中自定义配置 dao.properties
