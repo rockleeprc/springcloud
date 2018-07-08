e3ca7561540b9959f70ed9400989f7c4d1ec5217
Rock Lee <rockleeprc@outlook.com>
2018-07-08 20:55:08 +0800

    cache

--- spring-boot-mybatis/src/main/java/com/exam/config/RedisConfig.java
+++ spring-boot-mybatis/src/main/java/com/exam/config/RedisConfig.java
@@ -18,55 +18,55 @@ import org.springframework.data.redis.core.RedisTemplate;
 /**
  * Created by Administrator on 2018/7/8.
  */
-@Configuration
-public class RedisConfig extends CachingConfigurerSupport {
-    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
-    @Autowired
-    private Environment env;
-
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
-        redisConnectionFactory.setHostName(env.getProperty("spring.redis.host"));
-        redisConnectionFactory.setPort(Integer.parseInt(env.getProperty("spring.redis.port")));
-        return redisConnectionFactory;
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(cf);
-        return redisTemplate;
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-        cacheManager.setDefaultExpiration(600);
-        return cacheManager;
-    }
-
-    public CacheErrorHandler errorHandler() {
-        return new CacheErrorHandler() {
-            @Override
-            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
-                logger.warn("handleCacheGetError in redis: {}", exception.getMessage());
-            }
-
-            @Override
-            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
-                logger.warn("handleCachePutError in redis: {}", exception.getMessage());
-            }
-
-            @Override
-            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
-                logger.warn("handleCacheEvictError in redis: {}", exception.getMessage());
-            }
-
-            @Override
-            public void handleCacheClearError(RuntimeException exception, Cache cache) {
-                logger.warn("handleCacheClearError in redis: {}", exception.getMessage());
-            }
-        };
-    }
-}
+//@Configuration
+//public class RedisConfig extends CachingConfigurerSupport {
+//    private static final Logger logger = LoggerFactory.getLogger(RedisConfig.class);
+//    @Autowired
+//    private Environment env;
+//
+//    @Bean
+//    public JedisConnectionFactory redisConnectionFactory() {
+//        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+//        redisConnectionFactory.setHostName(env.getProperty("spring.redis.host"));
+//        redisConnectionFactory.setPort(Integer.parseInt(env.getProperty("spring.redis.port")));
+//        return redisConnectionFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(cf);
+//        return redisTemplate;
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate<?, ?> redisTemplate) {
+//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//        cacheManager.setDefaultExpiration(600);
+//        return cacheManager;
+//    }
+//
+//    public CacheErrorHandler errorHandler() {
+//        return new CacheErrorHandler() {
+//            @Override
+//            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+//                logger.warn("handleCacheGetError in redis: {}", exception.getMessage());
+//            }
+//
+//            @Override
+//            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+//                logger.warn("handleCachePutError in redis: {}", exception.getMessage());
+//            }
+//
+//            @Override
+//            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+//                logger.warn("handleCacheEvictError in redis: {}", exception.getMessage());
+//            }
+//
+//            @Override
+//            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+//                logger.warn("handleCacheClearError in redis: {}", exception.getMessage());
+//            }
+//        };
+//    }
+//}
