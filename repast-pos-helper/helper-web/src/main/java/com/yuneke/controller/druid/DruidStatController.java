48d50021e9b943c09f794ff7200f185302b20038
liyan <rockleeprc@outlook.com>
2018-08-14 16:41:02 +0800

    增加druid filter controller

---
+++ repast-pos-helper/helper-web/src/main/java/com/yuneke/controller/druid/DruidStatController.java
@@ -0,0 +1,17 @@
+package com.yuneke.controller.druid;
+
+import com.alibaba.druid.stat.DruidStatManagerFacade;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/datasource")
+public class DruidStatController {
+
+    @GetMapping("/stat")
+    public Object druidStat(){
+        // DruidStatManagerFacade#getDataSourceStatDataList 该方法可以获取所有数据源的监控数据，除此之外 DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
+        return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+    }
+}
--- repast-pos-helper/helper-web/src/main/java/com/yuneke/servlet/druid/DruidStatViewServlet.java
+++ repast-pos-helper/helper-web/src/main/java/com/yuneke/servlet/druid/DruidStatViewServlet.java
@@ -1,4 +1,4 @@
-package com.yuneke.servlet;
+package com.yuneke.servlet.druid;
 
 
 import com.alibaba.druid.support.http.StatViewServlet;
@@ -11,8 +11,8 @@ import javax.servlet.annotation.WebServlet;
  */
 @WebServlet(urlPatterns = "/druid/*",
         initParams={
-                @WebInitParam(name="allow",value="127.0.0.1"),// IP白名单 (没有配置或者为空，则允许所有访问)
-                @WebInitParam(name="deny",value="192.168.16.111"),// IP黑名单 (存在共同时，deny优先于allow)
+                //@WebInitParam(name="allow",value="127.0.0.1"),// IP白名单 (没有配置或者为空，则允许所有访问)
+                //@WebInitParam(name="deny",value="192.168.16.111"),// IP黑名单 (存在共同时，deny优先于allow)
                 @WebInitParam(name="loginUsername",value="admin"),// 用户名
                 @WebInitParam(name="loginPassword",value="geek"),// 密码
                 @WebInitParam(name="resetEnable",value="false")// 禁用HTML页面上的“Reset All”功能
