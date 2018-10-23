07ebfb35096bc0722683f3f87f9c76e8e162a128
liyan <rockleeprc@outlook.com>
2018-10-23 15:31:31 +0800

    springcloud-parent

--- springcloud-parent/eureka-7001/README.md
+++ springcloud-parent/eureka-7001/README.md
@@ -5,5 +5,11 @@
 ### yml
 * eureka.client.service-url.defaultZone：注册地址
 
-###
+### 注解
 * @EnableEurekaServer：开启Eureka服务器端
+
+### 配置类
+* EurekaServerConfigBean：服务端配置
+* EurekaClientConfigBean：客户端配置
+* EurekaInstanceConfigBean：实例元数据配置（描述自身服务的数据信息）
+    * InstanceInfo：包装EurekaInstanceConfigBean，发送给服务端
--- springcloud-parent/ribbon-consumer-9001/src/main/java/com/foo/controller/ConsumerController.java
+++ springcloud-parent/ribbon-consumer-9001/src/main/java/com/foo/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.foo.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +14,15 @@ public class ConsumerController {
     private RestTemplate restTemplate;
 
     @RequestMapping(path="/info",method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod ="infoFallback" )
     public String info() {
         return restTemplate.getForEntity("http://PROVIDER/hello/info", String.class).getBody();
     }
 
+
+    public String infoFallback(){
+        return "hystrix";
+    }
     @RequestMapping(path = "/foo")
     public boolean foo(){
         return true;
