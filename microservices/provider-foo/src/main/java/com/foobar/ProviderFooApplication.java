ddc1e249b474e70e6acb8febf99822b2e1e28c50
liyan <liyan1@kd.cn>
2018-12-25 11:52:47 +0800

    修改logback日志

--- microservices/provider-foo/src/main/java/com/foobar/ProviderFooApplication.java
+++ microservices/provider-foo/src/main/java/com/foobar/ProviderFooApplication.java
@@ -1,6 +1,8 @@
 package com.foobar;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,7 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @MapperScan("com.foobar.mapper")
 public class ProviderFooApplication {
+    private final static Logger logger = LoggerFactory.getLogger(ProviderFooApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ProviderFooApplication.class,args);
+        SpringApplication.run(ProviderFooApplication.class, args);
+        logger.info("==========provider foo start");
+        logger.debug("==========provider foo start");
     }
 }
