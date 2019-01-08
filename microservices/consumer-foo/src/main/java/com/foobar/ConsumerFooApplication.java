94ecf486372b4312ad810f905a31b732a09e98a5
liyan <rockleeprc@outlook.com>
2019-01-08 23:34:58 +0800

    auth

--- microservices/consumer-foo/src/main/java/com/foobar/ConsumerFooApplication.java
+++ microservices/consumer-foo/src/main/java/com/foobar/ConsumerFooApplication.java
@@ -3,15 +3,13 @@ package com.foobar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableHystrix
 public class ConsumerFooApplication {
     public static void main(String[] args) {
