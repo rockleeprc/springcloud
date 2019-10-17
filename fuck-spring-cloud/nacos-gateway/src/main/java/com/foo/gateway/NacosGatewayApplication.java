9b81ef1f81a820106428944be4fb985f87aa81eb
rockleeprc <rockleeprc@outlook.com>
2019-10-17 10:03:52 +0800

    gateway

---
+++ fuck-spring-cloud/nacos-gateway/src/main/java/com/foo/gateway/NacosGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.foo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosGatewayApplication.class, args);
+    }
+}
+
+
+/*
+
+ */
